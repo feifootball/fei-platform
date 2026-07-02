16d05a9c02d8a56d4268e515e02ce7dd2c2d0577
Daniela Portilla <danielaportilla@Laptop-de-Daniela.local>
2026-07-02 14:24:51 -0500

    Update FEI tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
       className={`${inter.variable} h-full font-sans antialiased`}
     >
       <head>
-        <link rel="icon" href="/favicon.svg?v=13" type="image/svg+xml" />
-        <link rel="shortcut icon" href="/favicon.ico?v=13" />
-        <link rel="apple-touch-icon" href="/apple-icon.svg?v=13" />
+        <link rel="icon" href="/favicon.svg?v=20" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.ico?v=20" />
+        <link rel="apple-touch-icon" href="/apple-icon.svg?v=20" />
       </head>
       <body className="min-h-full flex flex-col bg-fei-bg text-fei-text">
         {children}
